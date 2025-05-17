e7c36736be29ac1bd577a9625b759f736bdaefd9
HanHyunsoo <gustn4563@gmail.com>
2025-05-17 19:16:49 +0900

    feat: 로그인 기능 추가

--- apps/users/src/services/users.service.ts
+++ apps/users/src/services/users.service.ts
@@ -1,11 +1,16 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 import {
-  CreateUserRequestDto,
-  CreateUserResponseDto,
+  CreateOrLoginUserRequestDto,
+  CreateOrLoginUserResponseDto,
 } from '@event-reward-platform/protocol';
 import { User, UserDocument } from '../schemas/user.schema';
 
@@ -15,9 +20,9 @@ export class UsersService {
     @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
   ) {}
 
-  async signUp(
-    createUserRequest: CreateUserRequestDto,
-  ): Promise<CreateUserResponseDto> {
+  async create(
+    createUserRequest: CreateOrLoginUserRequestDto,
+  ): Promise<CreateOrLoginUserResponseDto> {
     const isExistUsername = await this.userModel.exists({
       userId: createUserRequest.userId,
     });
@@ -40,6 +45,73 @@ export class UsersService {
       },
       lastLoginAt: now,
     });
+
+    return {
+      userId: user.userId,
+      role: user.role,
+    };
+  }
+
+  async login(
+    loginUserRequest: CreateOrLoginUserRequestDto,
+  ): Promise<CreateOrLoginUserResponseDto> {
+    const user = await this.userModel.findOne(
+      { userId: loginUserRequest.userId },
+      {
+        userId: 1,
+        password: 1,
+        role: 1,
+        bannedUntil: 1,
+        consecutiveLogin: 1,
+        lastLoginAt: 1,
+        todayLoginCount: 1,
+      },
+    );
+
+    if (user == null) {
+      throw new RpcException(
+        new NotFoundException('유저가 존재하지 않습니다.'),
+      );
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      loginUserRequest.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new RpcException(
+        new UnauthorizedException('비밀번호가 일치하지 않습니다.'),
+      );
+    }
+
+    const now = new Date();
+
+    if (user.bannedUntil.getTime() > now.getTime()) {
+      throw new RpcException(
+        new UnauthorizedException(
+          `유저가 정지되었습니다.(정지 종료 시간: ${user.bannedUntil.toISOString()})`,
+        ),
+      );
+    }
+
+    const oneDay = 1000 * 60 * 60 * 24;
+    const isConsecutiveLogin =
+      Math.floor(user.lastLoginAt.getTime() / oneDay) -
+        Math.floor(user.consecutiveLogin.startTime.getTime() / oneDay) ===
+      1;
+
+    if (isConsecutiveLogin) {
+      user.todayLoginCount = 1;
+      user.consecutiveLogin.count = user.consecutiveLogin.count + 1;
+    } else {
+      user.consecutiveLogin.count = 1;
+      user.consecutiveLogin.startTime = now;
+    }
+
+    user.todayLoginCount = user.todayLoginCount + 1;
+    user.lastLoginAt = now;
+    await user.save();
 
     return {
       userId: user.userId,
--- libs/protocol/src/users/users.patterns.ts
+++ libs/protocol/src/users/users.patterns.ts
@@ -1,4 +1,5 @@
 export const USER_PATTERNS = {
   HEALTH_CHECK: 'users.healthCheck',
-  CREATE_USER: 'users.createUser',
+  CREATE_USER: 'users.create',
+  LOGIN: 'users.login',
 } as const;
