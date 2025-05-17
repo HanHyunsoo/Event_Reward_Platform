ddaa5af97bdcced2af5fd8f5928bf1a7780c138c
HanHyunsoo <gustn4563@gmail.com>
2025-05-17 16:35:05 +0900

    feat: 마이크로서비스, 게이트웨이 예외 필터 추가

--- apps/events/src/events.module.ts
+++ apps/events/src/events.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { MicroServiceExceptionFilter } from '@event-reward-platform/core';
+import { ClientsModule } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -9,8 +13,26 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: ['.env'],
     }),
+    ClientsModule.registerAsync([
+      {
+        name: 'USERS_SERVICE',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            port: configService.get('USER_SERVICE_PORT', 3001),
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
   ],
   controllers: [EventsController],
-  providers: [EventsService],
+  providers: [
+    EventsService,
+    {
+      provide: APP_FILTER,
+      useClass: MicroServiceExceptionFilter,
+    },
+  ],
 })
 export class EventsModule {}
--- apps/events/src/main.ts
+++ apps/events/src/main.ts
@@ -2,17 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { EventsModule } from './events.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
-import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
-  const app = await NestFactory.create(EventsModule);
-  const configService = app.get(ConfigService);
-  const port = parseInt(configService.get('EVENT_SERVICE_PORT', '3002'));
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: { port },
-  });
-
-  await app.startAllMicroservices();
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    EventsModule,
+    {
+      transport: Transport.TCP,
+      options: { port: parseInt(process.env.EVENT_SERVICE_PORT ?? '3002') },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
